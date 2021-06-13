96d8ff7cbe684226a044c33a4257ac1aaa9a6ece
Euclides <eneye280@gmail.com>
2021-06-13 17:32:25 -0500

    Add timer that tun outside you app

---
+++ Timers that run outside your App/Assets/Timers that run outside your App/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timerStart;
+    public float timer;
+
+    [SerializeField] TextMeshProUGUI tmpTimer;
+
+    private void Start()
+    {
+        timer -= TimerMaster.instance.CheckDate();
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        tmpTimer.text = timer.ToString();
+
+        if(timer <= 0)
+        {
+            timer = 0;
+            tmpTimer.text = "Rewards : " + timer.ToString();
+        }
+    }
+
+    public void SaveTimer()
+    {
+        ResetTimer();
+    }
+
+    private void ResetTimer()
+    {
+        TimerMaster.instance.SaveDate();
+        timer = timerStart;
+        timer -= TimerMaster.instance.CheckDate();
+    }
+
+    public void CheckTimer()
+    {
+        print(60 - TimerMaster.instance.CheckDate() + " in real seconds has passed");
+    }
+}
---
+++ Timers that run outside your App/Assets/Timers that run outside your App/Scripts/TimerMaster.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerMaster : MonoBehaviour
+{
+    public static TimerMaster instance;
+
+    DateTime currentData;
+    DateTime oldData;
+
+    public string saveLocation;
+
+    private void Awake()
+    {
+        instance = this;
+
+        saveLocation = "lastSaveDatei";
+
+    }
+
+    public float CheckDate()
+    {
+        currentData = System.DateTime.Now;
+        string tempString = PlayerPrefs.GetString(saveLocation, "1");
+
+        long tempLong = Convert.ToInt64(tempString);
+        DateTime oldDate = DateTime.FromBinary(tempLong);
+        print("Old Date : " + oldDate);
+
+        TimeSpan difference = currentData.Subtract(oldDate);
+        print("Difference: " + difference);
+
+        return (float)difference.TotalSeconds;
+
+    }
+
+    public void SaveDate()
+    {
+        PlayerPrefs.SetString(saveLocation,System.DateTime.Now.ToBinary().ToString());
+        print("Saving this date to player prefs: " + System.DateTime.Now);
+    }
+}
